8eb366355d0f0419e1cc747f4e506c438b0262f0
filentati <filen04@mail.ru>
2024-08-30 13:07:31 +0300

    Add files via upload

--- hw_arrays.py
+++ hw_arrays.py
@@ -1,13 +1,21 @@
+def move_zeros_to_end(arr):
+    new_array = []
+    counter = 0 
+    
+    for el in arr:
+        if el == 0:
+            counter += 1
+        else:
+            new_array.append(el)
+            
+    new_array.extend([0]*counter)
+    return ' '.join(map(str, new_array))
+
+
 size = int(input())
 input_array = input() 
-element = int(input())
 
 array = list(map(int, input_array.split()))
 
-counter = 0 
+print(move_zeros_to_end(array))
 
-for el in array:
-    if el == element:
-        counter+=1
-        
-print(size - counter)
